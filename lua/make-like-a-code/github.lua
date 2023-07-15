f25a943f7b54f158ba22887c1c6765a2470ed845
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2023-07-15 20:51:36 +0530

    fetch snippets from github based on commit sha

--- lua/make-like-a-code/github.lua
+++ lua/make-like-a-code/github.lua
@@ -1,2 +1,53 @@
 local curl = require('plenary.curl')
-print(vim.inspect(curl.get("https://api.chucknorris.io/jokes/random")))
+
+local function commit_url(repo, sha)
+    return "https://api.github.com/repos/" .. repo .. "/commits/" .. sha
+end
+
+local function raw_file_url(repo, sha, filename)
+    return "https://raw.githubusercontent.com/" .. repo .. "/" .. sha .. "/" .. filename
+end
+
+local function fetch(url)
+    local res = curl.get(url)
+    if res.status ~= 200 then
+        error()
+    end
+    return res.body
+end
+
+local function fetch_raw_files(repo, file_name, sha, parent_sha)
+    local ok, raw_file = pcall(fetch, raw_file_url(repo, sha, file_name))
+    if not ok then
+        error()
+    end
+
+    local ok_parent, raw_file_parent = pcall(fetch, raw_file_url(repo, parent_sha, file_name))
+    if not ok_parent then
+        error()
+    end
+
+    return raw_file, raw_file_parent
+end
+
+local function fetch_snippets(repo, sha)
+    local ok_commit, res_commit = pcall(fetch, commit_url(repo, sha))
+    if not ok_commit then
+        error()
+    end
+
+    local res_json = vim.json.decode(res_commit)
+    local parent_sha = res_json.parents[1].sha
+    local file_name = res_json.files[1].filename
+
+    local ok_files, raw_file, raw_file_parent = pcall(fetch_raw_files, repo, file_name, sha, parent_sha)
+    if not ok_files then
+        error()
+    end
+
+    return raw_file, raw_file_parent
+end
+
+return {
+    fetch_snippets = fetch_snippets
+}
--- lua/make-like-a-code/init.lua
+++ lua/make-like-a-code/init.lua
@@ -1,33 +1,31 @@
 local u = require('make-like-a-code.utils')
+local github = require('make-like-a-code.github')
 
-local prompt = [[
-Hi there
-how is it
-going?
-]]
+local ok, new_snippet, old_snippet = pcall(github.fetch_snippets, 'mebble/mcj', '8f091afc5d60a5a231ec05b7c1ae86a31a4bd9cb')
 
 local player_win = vim.api.nvim_get_current_win()
 local player_buf = vim.api.nvim_create_buf(true, true)
 vim.api.nvim_buf_set_name(player_buf, "Type here")
+vim.api.nvim_buf_set_lines(player_buf, 0, -1, false, u.split_string(old_snippet))
 
 vim.cmd('vsplit')
 
 local prompt_win = vim.api.nvim_get_current_win()
 local prompt_buf = vim.api.nvim_create_buf(true, true)
 vim.api.nvim_buf_set_name(prompt_buf, "Prompt")
+vim.api.nvim_buf_set_lines(prompt_buf, 0, -1, false, u.split_string(new_snippet))
 
 vim.api.nvim_win_set_buf(player_win, player_buf)
 vim.api.nvim_win_set_buf(prompt_win, prompt_buf)
-vim.api.nvim_buf_set_lines(prompt_buf, 0, -1, false, u.split_string(prompt))
 vim.api.nvim_set_current_win(player_win)
 
 local start_time = vim.fn.localtime()
 print('Starting game at buffer:', player_buf)
 vim.api.nvim_buf_attach(player_buf, true, {
-    on_lines = function (_, buf)
+    on_lines = function(_, buf)
         local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
         local typed = u.join_strings(lines)
-        if typed == prompt then
+        if typed == new_snippet then
             local end_time = vim.fn.localtime()
             local secs = end_time - start_time
             print('Finished in ' .. secs .. " seconds")

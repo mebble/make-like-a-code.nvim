9d12c7dafb569672d577b9b94c28542afc510dfd
Neil Syiemlieh <neilsyiemlieh@gmail.com>
2023-08-18 17:09:46 +0530

    [refactor] create functions for window and pane setup

--- lua/make-like-a-code/init.lua
+++ lua/make-like-a-code/init.lua
@@ -1,6 +1,40 @@
 local u = require('make-like-a-code.utils')
 local snippet = require('make-like-a-code.snippet')
 
+local function activate_game_panes(player_win, prompt_win)
+    vim.api.nvim_win_set_option(player_win, 'diff', true)
+    vim.api.nvim_win_set_option(prompt_win, 'diff', true)
+    vim.api.nvim_win_set_option(player_win, 'scrollbind', true)
+    vim.api.nvim_win_set_option(prompt_win, 'scrollbind', true)
+end
+
+local function deactivate_game_panes(player_win, prompt_win)
+    vim.api.nvim_win_set_option(player_win, 'diff', false)
+    vim.api.nvim_win_set_option(prompt_win, 'diff', false)
+    vim.api.nvim_win_set_option(player_win, 'scrollbind', false)
+    vim.api.nvim_win_set_option(prompt_win, 'scrollbind', false)
+end
+
+
+local function create_game_panes(old_snippet_lines, new_snippet_lines)
+    local player_win = vim.api.nvim_get_current_win()
+    local player_buf = vim.api.nvim_create_buf(true, true)
+    vim.api.nvim_buf_set_name(player_buf, "Type here")
+    vim.api.nvim_buf_set_lines(player_buf, 0, -1, false, old_snippet_lines)
+
+    vim.cmd('vsplit')
+
+    local prompt_win = vim.api.nvim_get_current_win()
+    local prompt_buf = vim.api.nvim_create_buf(true, true)
+    vim.api.nvim_buf_set_name(prompt_buf, "Prompt")
+    vim.api.nvim_buf_set_lines(prompt_buf, 0, -1, false, new_snippet_lines)
+
+    vim.api.nvim_win_set_buf(player_win, player_buf)
+    vim.api.nvim_win_set_buf(prompt_win, prompt_buf)
+
+    return player_win, player_buf, prompt_win, prompt_buf
+end
+
 local function start(github_repo, commit_hash)
     local ok, languages = pcall(snippet.get_languages)
     if not ok then
@@ -16,37 +50,22 @@ local function start(github_repo, commit_hash)
     local new_snippet_lines = u.split_string(new_snippet)
     local old_snippet_lines = u.split_string(old_snippet)
 
+    local player_win, player_buf, prompt_win  = create_game_panes(old_snippet_lines, new_snippet_lines);
+    activate_game_panes(player_win, prompt_win)
+
     -- HACK. TODO = ensure that when player_buf's lines are joined, they're the same as they were before splitting 
     new_snippet = u.join_strings(new_snippet_lines)
-
-    local player_win = vim.api.nvim_get_current_win()
-    local player_buf = vim.api.nvim_create_buf(true, true)
-    vim.api.nvim_buf_set_name(player_buf, "Type here")
-    vim.api.nvim_buf_set_lines(player_buf, 0, -1, false, old_snippet_lines)
-
-    vim.cmd('vsplit')
-
-    local prompt_win = vim.api.nvim_get_current_win()
-    local prompt_buf = vim.api.nvim_create_buf(true, true)
-    vim.api.nvim_buf_set_name(prompt_buf, "Prompt")
-    vim.api.nvim_buf_set_lines(prompt_buf, 0, -1, false, new_snippet_lines)
 
     local lang = snippet.extension_to_language(languages, file_ext)
     if lang ~= nil then
         print('Language: '..lang)
     end
 
-    vim.api.nvim_win_set_buf(player_win, player_buf)
-    vim.api.nvim_win_set_buf(prompt_win, prompt_buf)
-    vim.api.nvim_win_set_option(player_win, 'diff', true)
-    vim.api.nvim_win_set_option(prompt_win, 'diff', true)
-    vim.api.nvim_win_set_option(player_win, 'scrollbind', true)
-    vim.api.nvim_win_set_option(prompt_win, 'scrollbind', true)
 
     vim.api.nvim_set_current_win(player_win)
 
     local start_time = vim.fn.localtime()
-    print('Starting game at buffer:', player_buf)
+    print('Listening for changes at buffer:', player_buf)
     vim.api.nvim_buf_attach(player_buf, true, {
         on_lines = function(_, buf)
             local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
@@ -55,10 +74,7 @@ local function start(github_repo, commit_hash)
                 local end_time = vim.fn.localtime()
                 local secs = end_time - start_time
                 print('Finished in ' .. secs .. " seconds")
-                vim.api.nvim_win_set_option(player_win, 'diff', false)
-                vim.api.nvim_win_set_option(prompt_win, 'diff', false)
-                vim.api.nvim_win_set_option(player_win, 'scrollbind', false)
-                vim.api.nvim_win_set_option(prompt_win, 'scrollbind', false)
+                deactivate_game_panes(player_win, prompt_win)
                 return true
             end
         end
